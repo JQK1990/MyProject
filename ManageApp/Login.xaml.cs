261027ccf236dfbe0b68905338e2680833b5ab8f
jqk <136359076@qq.com>
2017-06-23 18:35:58 +0800

    登录

---
+++ ManageApp/Login.xaml.cs
@@ -0,0 +1,21 @@
+﻿
+using System.Windows;
+
+namespace ManageApp
+{
+    /// <summary>
+    /// UserControl1.xaml 的交互逻辑
+    /// </summary>
+    public partial class Login
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLogin_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
